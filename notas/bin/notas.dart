643bb8dce2e50eb937fcf14d16321ee2b6ccfc20
Matheus Scarparo <mat.scarparo@gmail.com>
2024-11-12 17:38:03 -0300

    Initial commit

---
+++ notas/bin/notas.dart
@@ -0,0 +1,9 @@
+void main() {
+
+}
+
+String getComando() {
+  print("Digite um comando: 1 - Adicionar nota, 2 - Listar notas, 3 - Sair");
+  List<String> comandos = <String>["1", "2", "3"];
+  String? entrada = "";
+}
